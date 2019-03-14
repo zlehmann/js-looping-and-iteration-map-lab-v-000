0b1e0111a8797714b2e9773e44d99bed7a517c13
Zachary Lehmann <zlehmann@gmail.com>
2019-03-14 23:53:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function lowerCaseDrivers(drivers) {
   const newDrivers = drivers.map(function(driver) { return driver.toLowerCase();});
   return newDrivers;
 }
+
+function nameToAttributes(drivers) {
+  const newDrivers = drivers.map(function(driver) { return Object.assign({}, driver, { firstName: driver.split(' ')[0], lastName: driver.split(' ')[1];});});
+  return newDrivers;
+}
