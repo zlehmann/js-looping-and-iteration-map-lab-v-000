59ecdf9522e189d4929fac4a55413650454867c4
Zachary Lehmann <zlehmann@gmail.com>
2019-03-14 23:47:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers) {
-
+  const newDrivers = drivers.map(function(driver) { return driver.toLowerCase();});
+  return newDrivers;
 }
